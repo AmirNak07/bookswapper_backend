128c7f2664d4ae4f55b09a91d0ef6749c35efac3
asalde_le1 <57564314+asaldele1@users.noreply.github.com>
2024-07-03 07:12:42 +0000

    sql: Make changes to exchanges table
    Change-Id: If3837ba4fe6cec886397139a28d0b13b6ea1930e

--- backend/book_swapper/migrations/create_tables/create_exchanges_table.up.sql
+++ backend/book_swapper/migrations/create_tables/create_exchanges_table.up.sql
@@ -1,10 +1,11 @@
 CREATE TABLE IF NOT EXISTS exchanges
 (
     id          SERIAL PRIMARY KEY,
-    author_id INTEGER,
-    FOREIGN KEY (author_id) REFERENCES users (id),
-    book_name   VARCHAR(75),
+    user_id INTEGER,
+    FOREIGN KEY (user_id) REFERENCES users (id),
+    book_name   VARCHAR(100),
     description VARCHAR(250) DEFAULT '',
+    author VARCHAR(75) DEFAULT '',
     date        DATE NOT NULL,
     time        TIME NOT NULL
 );
