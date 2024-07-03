f19c896484a9a0c4dde55f17f62ccc22e02fb544
asalde_le1 <57564314+asaldele1@users.noreply.github.com>
2024-07-03 15:55:06 +0300

    Modify db

--- backend/book_swapper/migrations/create_tables/create_exchanges_table.up.sql
+++ backend/book_swapper/migrations/create_tables/create_exchanges_table.up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE IF NOT EXISTS exchanges
 (
     id          SERIAL PRIMARY KEY,
-    user_id INTEGER,
+    user_id     INTEGER,
     FOREIGN KEY (user_id) REFERENCES users (id),
-    book_name   VARCHAR(100),
+    book_name   VARCHAR(100) NOT NULL,
     description VARCHAR(250) DEFAULT '',
-    author VARCHAR(75) DEFAULT '',
+    author      VARCHAR(75) NOT NULL,
     date        DATE NOT NULL,
     time        TIME NOT NULL
 );
